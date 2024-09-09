84dfdb05479413187b58b5174847c2b022b214ef
Steven Lin <yuchenglin0705@gmail.com>
2024-09-09 18:00:23 -0400

    my refactoring has succeeded successfully

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -30,5 +30,31 @@ class FizzBuzz {
 
             }
         }
+
+        int i = 1;
+        while (i < 100) {
+            i = doFizzy(i);
+        }
+    }
+
+    private static int doFizzy(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        }
+        else if (divisibleBy3) {
+            System.out.println("Fizz");
+        }
+        else if (divisibleBy5) {
+            System.out.println("Buzz");
+        }
+        else {
+            System.out.println(i);
+        }
+        i++;
+        return i;
+
     }
 }
